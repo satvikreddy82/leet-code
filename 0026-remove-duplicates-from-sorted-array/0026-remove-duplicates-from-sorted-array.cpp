3ea6831fc822d79044418d17be3737e073ee22e3
Vaddi Hima Satvik Reddy <satvikreddy82@gmail.com>
2026-07-22 19:24:54 +0530

    Time: 0 ms (100%), Space: 22.6 MB (53.75%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int index=1;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]!=nums[i+1]){
+                nums[index]=nums[i+1];
+                index++;
+            }
+        }
+        return index;
+    }
+};
