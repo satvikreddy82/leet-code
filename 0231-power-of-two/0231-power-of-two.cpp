1ab3228b5cc62200ad83e691b6bc94709f7b450f
Vaddi Hima Satvik Reddy <satvikreddy82@gmail.com>
2026-08-18 22:39:28 +0530

    Time: 0 ms (100%), Space: 7.9 MB (50.48%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        while(n>1){
+            if(n%2==0) n/=2;
+            else return false;
+        }
+        if(n==1) return true;
+        return false;
+    }
+};
