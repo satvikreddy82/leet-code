101d64abc855f0f58425288171a5e82fb97db7e7
Vaddi Hima Satvik Reddy <satvikreddy82@gmail.com>
2026-08-21 00:12:01 +0530

    Time: 8 ms (14.92%), Space: 8.9 MB (75.29%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-      while(n>1){
-        if(n%3==0) n/=3;
-        else return false;
-         }
-    if(n==1) return true;
-    return false;
- }
+        if (n < 1) return false;
+        while (n % 3 == 0) {    
+            n /= 3;
+        }
+        return n == 1;         
+    }
 };
