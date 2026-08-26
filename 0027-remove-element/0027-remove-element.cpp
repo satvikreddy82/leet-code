f60753d83bb205eed41b83f4305653dd0ad5ed2b
Vaddi Hima Satvik Reddy <satvikreddy82@gmail.com>
2026-08-27 00:04:19 +0530

    Time: 0 ms (100%), Space: 11.7 MB (49.04%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        vector<int> ans;
-        int index=0,count=0;
-        for(int num:nums){
-            if(num!=val){
-                ans.push_back(num);
-                index++;
+        int index = 0; // Pointer to place the next valid element
+
+        for (int i = 0; i < nums.size(); i++) {
+            // If the current element is NOT the value to remove
+            if (nums[i] != val) {
+                nums[index] = nums[i]; // Move it forward
+                index++;               // Move the write pointer
             }
-            else count++;
         }
-        for(int i=index;count!=0;i++){
-            ans.push_back(val);
-            count--;
-        }
-        nums=ans;
-        return index;
+        
+        return index; // index represents the count of valid elements
     }
 };
